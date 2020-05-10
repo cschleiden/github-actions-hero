15c2275f120d0aa4d9fa0de34131c8745f6ca01e
Christopher Schleiden <cschleiden@live.de>
2020-05-09 21:41:22 -0700

    Disable schedule

--- src/lessons/runSchedule.ts
+++ src/lessons/runSchedule.ts
@@ -1,18 +1,18 @@
-import { Lesson } from "./lesson";
+// import { Lesson } from "./lesson";
 
-export const runSchedule: Lesson = {
-  title: `Scheduled workflows`,
+// export const runSchedule: Lesson = {
+//   title: `Scheduled workflows`,
 
-  description: `Workflows can also run at specific times. GitHub Actions supports [POSIX cron syntax](https://help.github.com/en/actions/reference/workflow-syntax-for-github-actions#onschedule). Workflows can run at most every 5 minutes.`,
+//   description: `Workflows can also run at specific times. GitHub Actions supports [POSIX cron syntax](https://help.github.com/en/actions/reference/workflow-syntax-for-github-actions#onschedule). Workflows can run at most every 5 minutes.`,
 
-  workflow: `on:
-  schedule:
-%    - cron:  ''`,
+//   workflow: `on:
+//   schedule:
+// %    - cron:  ''`,
 
-  events: [
-    {
-      event: "schedule",
-      cron: "*/15 * * * *",
-    },
-  ],
-};
+//   events: [
+//     {
+//       event: "schedule",
+//       cron: "*/15 * * * *",
+//     },
+//   ],
+// };
