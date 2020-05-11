5e611acfa633aa3af4353cf6a3d3fdfc4b0ce8c9
Christopher Schleiden <cschleiden@live.de>
2020-05-10 18:23:57 -0700

    Add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,14 @@
+import Router from "next/router";
+import { useEffect } from "react";
 import "../../styles/main.css";
+import { initGA, logPageView } from "../analytics";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    initGA();
+    logPageView();
+    Router.events.on("routeChangeComplete", logPageView);
+  }, []);
+
   return <Component {...pageProps} />;
 }
