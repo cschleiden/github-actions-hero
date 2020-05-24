6544b825179e20c9136481c0922e2707474db1fc
Christopher Schleiden <cschleiden@live.de>
2020-05-23 17:27:55 -0700

    Fix exports

--- packages/github-actions-interpreter/src/index.ts
+++ packages/github-actions-interpreter/src/index.ts
@@ -1,12 +1,14 @@
+import { RuntimeContexts } from "./lib/expressions";
+
 export { WorkflowExecution } from "./components/workflowExecution";
 export {
   evaluateExpression,
   ExpressionError,
   parseExpression,
   replaceExpressions,
-  RuntimeContext,
 } from "./lib/expressions/index";
 export { parse, ParseError } from "./lib/parser/parser";
 export { run, RunError } from "./lib/runner/runner";
 export * from "./lib/runtimeModel";
 export * from "./lib/workflow";
+export type { RuntimeContexts };
