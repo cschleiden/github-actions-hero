941a9ef03a3e3ce858fb6dbd1848c594430bed08
Yukai Chou <muzimuzhi@gmail.com>
2023-04-11 13:08:22 +0800

    Fix typo in lessons

--- src/lessons/runShellScript.ts
+++ src/lessons/runShellScript.ts
@@ -3,7 +3,7 @@ import { Lesson } from "./lesson";
 export const runShellScript: Lesson = {
   title: `Execute shell scripts`,
 
-  description: `So far our workflows have just \`echo\`'d \`"Success!\`. That's great but doesn't provide that much value. Often you want build some code, or deploy a service. With [\`run\`](https://help.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idstepsrun) steps you can execute arbitrary shell commands.
+  description: `So far our workflows have just \`echo\`'d \`"Success!"\`. That's great but doesn't provide that much value. Often you want build some code, or deploy a service. With [\`run\`](https://help.github.com/en/actions/reference/workflow-syntax-for-github-actions#jobsjob_idstepsrun) steps you can execute arbitrary shell commands.
 
 As an example, let's have our workflow execute the \`./scriptDeploy.sh\` shell script.`,
 
